05aaae799ad69167fdbe1905d86d5c20b93898c0
Apisit Lee <apisit-lee@outlook.com>
2025-03-08 13:28:56 +0800

    upd config

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,8 +3,20 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>安阳古文</span>,
+  search: {
+    placeholder: '全文搜索'
+  },
+  sidebar: {
+    toggleButton: true,
+  },
+  editLink: {
+    component: null,
+  },
+  feedback: {
+    content: null,
+  },
   footer: {
-    text: '安阳古文',
+    component: null,
   },
 }
 
